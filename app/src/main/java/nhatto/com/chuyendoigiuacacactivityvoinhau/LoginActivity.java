2578ee37e5b650f5673355a821a317c684c10493
hoangcoder96 <nhattocoder@gmail.com>
2017-04-20 20:42:38 +0700

    Initial commit

---
+++ app/src/main/java/nhatto/com/chuyendoigiuacacactivityvoinhau/LoginActivity.java
@@ -0,0 +1,30 @@
+package nhatto.com.chuyendoigiuacacactivityvoinhau;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by User name on 20/04/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
